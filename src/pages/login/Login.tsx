5aa29514991f6a2b206a5c319b806289f67ac749
yeooong-dev <yeooong12@naver.com>
2023-09-08 06:22:09 +0900

    login 완료

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,63 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContent, AuthWrap } from "../../components/register/StRegister";
+import { Logo } from "../../components/wrapper/StWrapper";
+import { EmailInputLogin, LoginBtn, PwInputLogin } from "./StLogin";
+import useInput from "../../hook/useInput";
+import { useCookies } from "react-cookie";
+import { login } from "../../api/auth";
 
 function Login() {
-    return (
-        <>
-            <Link to='/register'>회원가입</Link>
-        </>
-    );
+  const [emailValue, setEmailValue] = useInput();
+  const [pwValue, setPwValue] = useInput();
+  const [cookies, setCookie] = useCookies(["authorization"]);
+
+  const navigate = useNavigate();
+
+  const onClickLoginBtn = () => {
+    if (!emailValue || !pwValue) {
+      alert("이메일과 비밀번호를 입력바랍니다.");
+    }
+
+    login({ email: emailValue, pw: pwValue })
+      .then((res) => {
+        const authId = res.data.Authorization;
+        setCookie("authorization", "Bearer" + authId);
+        alert("로그인이 완료되었습니다.");
+        navigate("/main");
+      })
+      .catch((err) => {
+        alert(err.response.data.message || "다시 입력해주세요.");
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <AuthWrap>
+        <AuthContent>
+          <Link to='/'>
+            <Logo>PORT</Logo>
+          </Link>
+          <EmailInputLogin
+            type='text'
+            value={emailValue}
+            onChange={setEmailValue}
+            placeholder='아이디 입력'
+          />
+          <PwInputLogin
+            type='password'
+            value={pwValue}
+            onChange={setPwValue}
+            placeholder='비밀번호 입력'
+          />
+          <LoginBtn onClick={onClickLoginBtn}>로그인</LoginBtn>
+          <Link to='/register' style={{ color: "black", fontSize: "1.1rem" }}>
+            회원가입
+          </Link>
+        </AuthContent>
+      </AuthWrap>
+    </>
+  );
 }
 
 export default Login;
---
+++ src/pages/login/StLogin.ts
@@ -0,0 +1,33 @@
+import { styled } from "styled-components";
+
+export const EmailInputLogin = styled.input`
+  width: 380px;
+  height: 55px;
+  background: #f6f6f6;
+  padding-left: 1rem;
+  font-size: 1rem;
+  margin-top: 35px;
+  margin-bottom: 15px;
+  padding-left: 2rem;
+`;
+
+export const PwInputLogin = styled.input`
+  width: 380px;
+  height: 55px;
+  background: #f6f6f6;
+  padding-left: 1rem;
+  font-size: 1rem;
+  margin-bottom: 20px;
+  padding-left: 2rem;
+`;
+
+export const LoginBtn = styled.button`
+  width: 410px;
+  height: 55px;
+  margin-top: 10px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  background: #51439d;
+  color: white;
+  font-size: 1.1rem;
+`;
